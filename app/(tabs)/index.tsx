caec654f8ceb08ea5a08875763e860ad4e3f3a8f
My <my.brindesjo@gmail.com>
2025-06-10 14:21:52 +0200

    Remove unused components and clean up Explore tab layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,7 @@
 import { Image } from "expo-image";
-import { Platform, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 
-import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
 import LoginForm from "@/components/LoginForm"; // Absolut import med alias
